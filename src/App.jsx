e635f5e138e2c064caa9cea89eebae7376269394
shad-bin-abi-aydid <shadaydid@gmail.com>
2025-03-15 07:26:10 +0000

    Project start with setup tailwind and daisyUI

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     
+      <h1 className='text-xl text-center font-bold bg-rose-500'>Vite + React</h1>
+
+      
+      
+    </>
+  )
+}
+
+export default App
